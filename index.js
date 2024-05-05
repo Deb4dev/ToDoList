06d7a2210b189230950dce4f41a9fc30b6f8e68d
Deb4Dev <chakrabortydebargha575@gmail.com>
2024-05-05 18:25:04 +0530

    inti

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 const input = document.getElementById('input')
 const addBtn = document.getElementById('addButton')
+const taskList = document.getElementById('taskList')
+function add(){
+    const task = input.value.trim()
+    if(task){
+        createTaskelem(task)
+        input.value = ""
+    }else{
+        alert("give a proper task")
+    }
+}
+addBtn.addEventListener('click',add)
+
+const createTaskelem = (task)=>{
+    const list = document.createElement('li')
+    list.textContent = task
+    taskList.appendChild(list)
+}
+
+const saveTask = ()=>{
+    let tasks = []
+    taskList.querySelectorAll('li').forEach((item)=>{
+        tasks.push(item.textContent.trim())
+        
+    })
+    localStorage.setItem('tasks',JSON.stringify(tasks))
+    
+}
